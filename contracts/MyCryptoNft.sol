31675d8dd88b8de7a2c21fd7381d25ed7d826abe
mastercodercat <mastercoderoy64@gmail.com>
2020-12-21 12:30:20 -0800

    add mintNFT function & onlyOwner modifier

--- contracts/MyCryptoNft.sol
+++ contracts/MyCryptoNft.sol
@@ -2,9 +2,28 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyCryptoNft is ERC721 {
+contract MyCryptoNft is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     constructor(string memory name, string memory symbol)
         ERC721(name, symbol)
     {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
